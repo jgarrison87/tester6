5faeef07066766e86d55661081110e1e9bf212f6
jgarrison87 <64297933+jgarrison87@users.noreply.github.com>
2020-09-23 16:49:44 -0400

    Add files via upload

--- forms/become-more.js
+++ forms/become-more.js
@@ -1,19 +1,56 @@
-function FadeInSection(props) {
-  const [isVisible, setVisible] = React.useState(true);
-  const domRef = React.useRef();
-  React.useEffect(() => {
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => setVisible(entry.isIntersecting));
-    });
-    observer.observe(domRef.current);
-    return () => observer.unobserve(domRef.current);
-  }, []);
-  return (
-    <div
-      className={`get-started ${isVisible ? "is-visible" : ""}`}
-      ref={domRef}
-    >
-      {props.children}
-    </div>
-  );
+// function FadeInSection(props) {
+//   const [isVisible, setVisible] = React.useState(true);
+//   const domRef = React.useRef();
+//   React.useEffect(() => {
+//     const observer = new IntersectionObserver((entries) => {
+//       entries.forEach((entry) => setVisible(entry.isIntersecting));
+//     });
+//     observer.observe(domRef.current);
+//     return () => observer.unobserve(domRef.current);
+//   }, []);
+//   return (
+//     <div
+//       className={`get-started ${isVisible ? "is-visible" : ""}`}
+//       ref={domRef}
+//     >
+//       {props.children}
+//     </div>
+//   );
+// }
+
+
+const closeBtn = document.querySelector(".email-modal-close");
+const submitBtn = document.querySelector(".submit");
+const ctaSubmitBtn = document.querySelector(".cta-submit");
+const ctaCloseBtn = document.querySelector(".cta-modal-close");
+
+// email modal
+submitBtn.addEventListener('click', () => {
+  document.getElementById("email-modal-bg").classList.remove("email-modal-active");
+});
+
+closeBtn.addEventListener('click', () => {
+  document.getElementById("email-modal-bg").classList.remove("email-modal-active");
+});
+
+
+function modalOpen10() {
+  document.getElementById("email-modal-bg").classList.add("email-modal-active");
 }
+
+setTimeout("modalOpen10()", 10000);
+
+// cta modal
+ctaSubmitBtn.addEventListener('click', () => {
+  document.getElementById('cta-modal-bg').classList.remove('cta-modal-active');
+});
+
+ctaCloseBtn.addEventListener('click', () => {
+  document.getElementById("cta-modal-bg").classList.remove("cta-modal-active");
+});
+
+function modalOpen60() {
+  document.getElementById('cta-modal-bg').classList.add('cta-modal-active');
+}
+
+setTimeout("modalOpen60()", 60000);
---
+++ forms/funnel.js
@@ -0,0 +1,7 @@
+// Script for range input label
+
+var rangeLabel = document.getElementById("range-label");
+var experience = document.getElementById("experience");
+function change() {
+  rangeLabel.innerText = experience.value + "K";
+}
